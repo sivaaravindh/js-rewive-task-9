f048510eafd090d6a2f10b3e2200b013205bb4c3
sivaaravindh <sivaaravind262003@gmail.com>
2023-01-17 17:09:26 +0530

    index

--- main.js
+++ main.js
@@ -1,36 +1,29 @@
-// let btn1=document.getElementById("btn");
-// btn1.addEventListener("click",myFunct);
+let btn1=document.getElementById("btn");
+btn1.addEventListener("click",myFunct);
 
-// function myFunct(){
-// var sub1=parseInt (document.getElementById("tamil").value);
-// var sub2=parseInt (document.getElementById("english").value);
-// var sub3=parseInt (document.getElementById("maths").value);
-// var sub4=parseInt (document.getElementById("science").value);
-// var sub5=parseInt (document.getElementById("social").value);
+function myFunct(){
+var age=parseInt (document.getElementById("age").value);
+let sex=confirm("are you female");
 
-// var x=sub1+sub2+sub3+sub4+sub5;
-// var a=x/ 5;
-
-// console.log(a);
-
-// if(a>90){
-//  document.getElementById("total").value=a;
-// document.getElementById("grate").innerText="A+ " +a;
-
+if(age>=20 && sex){
+  document.getElementById("con").innerText="work only urban areas";
+      
+}
+else if(age>=20 && age<=40 ){
+  document.getElementById("con").innerText="the man work anywhere";
  
-// }
-// else if(a<90 && a>=80){
-//   document.getElementById("total").value=a;
-//   document.getElementById("grate").innerText="A ";
-// }
-// else if(a<80 && a>=70){
-//   document.getElementById("total").value=a;
-//   document.getElementById("grate").innerText=" B";
-// }
-// else if(a<70 && a>=60){
-//   document.getElementById("total").value=a;
-//   document.getElementById("grate").innerText="C ";
-// }
+}
+else if(age>40 && age<=60 ){
+  document.getElementById("con").innerText=" the man work only urban areas";
+  
+}
+else{
+  document.getElementById("con").innerText="ERROR"
+ 
+}
+
+}
+
 // else if(a<60 && a>=40){
 //   document.getElementById("total").value=a;
 //   document.getElementById("grate").innerText="D ";
@@ -44,17 +37,3 @@
 // }
 
 
-let age=parseInt(prompt("enter the age"));
-let sex=confirm("are you female");
-if(age>=20 && sex){
-        console.log("work only urban areas");
-}
-else if(age>=20 && age<=40){
-  console.log("the man work anywhere");
-}
-else if(age>40 && age<=60){
-  console.log(" the man work only urban areas");
-}
-else{
-  console.log("ERROR");
-}
